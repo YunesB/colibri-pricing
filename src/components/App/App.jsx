086c559de78753b58259f1cbf9037faedb97da4b
Yunes B <yunes.bakhush@gmail.com>
2021-04-13 19:27:23 +0600

    alpha version V1.1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ import Header from '../Header/Header';
 import PriceList from '../PriceList/PriceList';
 import Footer from '../Footer/Footer';
 import * as PRICES from '../../database/prices';
-import { Switch, Route, withRouter } from 'react-router-dom';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 
 function App() {
   return (
@@ -14,22 +14,29 @@ function App() {
           <Route path="/therapy">
             <PriceList
               data = {PRICES.therapy}
+              heading="Терапия"
             />
           </Route>
           <Route path="/surgery">
             <PriceList
               data = {PRICES.surgery}
+              heading="Хирургия"
             />
           </Route>
           <Route path="/orthopedics">
             <PriceList
               data = {PRICES.orthopedics}
+              heading="Ортопедия"
             />
           </Route>
           <Route path="/orthodontics">
             <PriceList
               data = {PRICES.orthodontics}
+              heading="Ортодонтия"
             />
+          </Route>
+          <Route path="*">
+            <Redirect to="/therapy" />
           </Route>
         </Switch>
         <Footer />
--- src/components/PriceList/PriceList.jsx
+++ src/components/PriceList/PriceList.jsx
@@ -4,19 +4,22 @@ import ListItem from '../ListItem/ListItem';
 function PriceList(props) {
 
   return (
-    <ul className="prices">
-      <li className="price-item price-item_heading">
-        <p className="price-item__name">Наименование услуги</p>
-        <p className="price-item__sum">Цена (тенге)</p>
-      </li> 
-      {props.data
-          .map((item) => (
-            <ListItem
-              card={item}
-              key={item.name}
-            />
-        ))}
-    </ul>
+    <section className="price-list">
+      <h1 className="price-list__heading">{props.heading}</h1>
+      <ul className="prices">
+        <li className="price-item price-item_heading">
+          <p className="price-item__name">Наименование услуги</p>
+          <p className="price-item__sum">Цена (тенге)</p>
+        </li> 
+        {props.data
+            .map((item) => (
+              <ListItem
+                card={item}
+                key={item.name}
+              />
+          ))}
+      </ul>
+    </section>
   );
 }
 
