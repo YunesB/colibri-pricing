2ec38df12968279216bdf7bdfefd7cae07e3d76f
Yunes B <yunes.bakhush@gmail.com>
2021-04-16 12:29:58 +0600

    alpha version V1.3

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,18 +2,12 @@ function Footer() {
 
   return (
     <footer className="footer">
-      <a href="https://github.com/YunesB" className="footer__credit">&copy; 2021 Yunes Bakhush</a>
-      {/* <ul className='footer__links'>
-        <li className="footer__list-item">
-
-        </li>
-        <li className="footer__list-item">
-          
-        </li>
-        <li className="footer__list-item">
-        
-        </li>
-      </ul> */}
+      <p className="footer__notice">Цены могут отличаться от актуальных. Итоговые цены уточняйте у сотрудников клиники.</p>
+      <a href="https://github.com/YunesB" 
+        className="footer__credit hvr-underline-from-center" 
+        target="_blank" 
+        rel="noreferrer"
+      >&copy; 2021 Yunes Bakhush</a>
     </footer>
   );
 }
